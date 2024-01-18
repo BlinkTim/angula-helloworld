cfce0cf797719e0cd0870407eb1cf546bcb5b113
BlinkTim <zetteronid@gmail.com>
2024-01-18 19:27:02 +0100

    added item

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +12,29 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'todo';
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+    
+};
 }
